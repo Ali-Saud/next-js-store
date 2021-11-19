a8ab767250edc5098ef5224746fd7e1be71cec30
Ali-Saud <ali_saoud2000@yahoo.com>
2021-11-20 00:30:07 +0200

    Initial Commit - Layout & Styles & Material-UI implementation

---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            name: 'Free Shirt',
+            category: 'Shirts',
+            image: '/images/shirts1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular t-shirts with good quality' 
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/shirts2.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular Shirt' 
+        },
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/shirts3.jpg',
+            price: 90,
+            brand: 'Raymond',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A relaxing Shirt with nice looks' 
+        },
+        {
+            name: 'Golf Pants',
+            category: 'Pants',
+            image: '/images/pants1.jpg',
+            price: 90,
+            brand: 'Oliver',
+            rating: 4.0,
+            numReviews: 20,
+            countInStock: 20,
+            description: 'Smart looking pants' 
+        },
+        {
+            name: 'Fit Pants',
+            category: 'Pants',
+            image: '/images/pants2.jpg',
+            price: 95,
+            brand: 'Zara',
+            rating: 4.2,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'Popular & affordable pants ' 
+        },
+        {
+            mane: 'Classic Pants',
+            category: 'Pants',
+            image: '/images/pants3.jpg',
+            price: 75,
+            brand: 'Casely',
+            rating: 4.0,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'Popular Casely pants for youth' 
+        },
+    ]
+};
+
+export default data;
